e1eb114d95d0d7b69af838d3021ad4477068e492
Liam Rella <liam.rella@gmail.com>
2021-01-12 22:23:10 +1030

    add twig multi tags for Craft CMS

--- prettier.config.js
+++ prettier.config.js
@@ -4,5 +4,11 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  plugins: ['./node_modules/prettier-plugin-twig-melody']
+  plugins: ['./node_modules/prettier-plugin-twig-melody'],
+  twigMultiTags: [
+    'nav,endnav',
+    'switch, case,default,endswitch',
+    'ifchildren,endifchildren',
+    'cache,endcache'
+  ]
 };
