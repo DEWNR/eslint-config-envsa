3c65646954f3897fa44a44d9c96c0e3264ebf822
Liam Rella <liam.rella@gmail.com>
2021-05-25 10:25:49 +0930

    update env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true
+    es2021: true
   },
   plugins: [],
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
