b6fa5223db6a9bb57f94f8ff20eeb27803184adc
Liam Rella <liam.rella@gmail.com>
2021-05-25 09:46:58 +0930

    remove twig linting

--- prettier.config.js
+++ prettier.config.js
@@ -3,13 +3,5 @@ module.exports = {
   useTabs: false,
   tabWidth: 2,
   semi: true,
-  singleQuote: true,
-  plugins: ['./node_modules/prettier-plugin-twig-melody'],
-  twigMultiTags: [
-    'nav,endnav',
-    'switch, case,default,endswitch',
-    'ifchildren,endifchildren',
-    'cache,endcache'
-  ],
-  twigPrintWidth: 100
+  singleQuote: true
 };
