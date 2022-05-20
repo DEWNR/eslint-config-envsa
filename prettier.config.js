fa795d523f155d27488ca231990a3f3c1956a4fa
Liam Rella <liam.rella@gmail.com>
2022-05-20 15:06:46 +0930

    remove prettier plugins

--- prettier.config.js
+++ prettier.config.js
@@ -14,15 +14,4 @@ module.exports = {
       },
     },
   ],
-  plugins: ['./node_modules/prettier-plugin-twig-melody'],
-  twigPrintWidth: 120,
-  twigSingleQuote: false,
-  twigMultiTags: [
-    'nav,endnav',
-    'minify,endminify',
-    'switch,case,default,endswitch',
-    'ifchildren,endifchildren',
-    'cache,endcache',
-    'js,endjs',
-  ],
 };
