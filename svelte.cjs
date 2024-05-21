5691897ade3b42c8a3c0aff33d9dd86d6aef883d
Liam Rella <liam.rella@gmail.com>
2024-05-21 15:49:25 +0930

    turn off no mutable exports for svelte files

--- svelte.cjs
+++ svelte.cjs
@@ -1,5 +1,13 @@
 /** @type {import('eslint').Linter.BaseConfig} */
 module.exports = {
   extends: ['eslint:recommended', 'plugin:svelte/recommended'],
-  overrides: [{ files: ['*.svelte'], parser: 'svelte-eslint-parser' }],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      rules: {
+        'import/no-mutable-exports': 'off',
+      },
+    },
+  ],
 };
